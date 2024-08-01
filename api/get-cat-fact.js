6ea0b9ff2d8fa7287902dd39788404642753fd31
hrvyCased <salinas.johnharvey11@gmail.com>
2024-08-02 07:54:27 +0800

    try handle

--- api/get-cat-fact.js
+++ api/get-cat-fact.js
@@ -45,6 +45,8 @@ export default async function handler(req, res) {
 
         previousFacts.push(fact);
         res.status(200).json({ fact: fact });
+        attempts = 0;
+        
         
     } catch (error) {
         console.error("Error occurred:", error);
