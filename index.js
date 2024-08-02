2e158c26633edf0b9afa43d6e3599f0d709aea24
hrvyCased <salinas.johnharvey11@gmail.com>
2024-08-02 14:45:36 +0800

    Massive

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const loading = [
     "wait, me thinking..."
 ];
 
-
-document.querySelector('.generate-button').addEventListener('click', async () => {
+const genButton = document.querySelector('.generate-button');
+genButton.addEventListener('click', async () => {
+    const catBox = document.querySelector('.cat-box');
     const factElement = document.querySelector('.fact-text');
     factElement.textContent = loading[Math.floor(Math.random() * loading.length)];
     const meow = new Audio('Cats-loud-meow-sound-clip.mp3');
@@ -16,24 +17,35 @@ document.querySelector('.generate-button').addEventListener('click', async () =>
         const response = await fetch("/api/get-cat-fact");
         const data = await response.json();
         factElement.classList.remove('generated');
+        catBox.classList.remove('yapping');
         void factElement.offsetWidth;
         factElement.textContent = data.fact;
         factElement.classList.add('generated');
-
+        catBox.classList.add('yapping');
+        setTimeout(()=>{
+            catBox.classList.remove('yapping');
+        }, 1690);
         meow.play();
         meow.addEventListener('ended', function() {
             meow.remove();
         });
+        genButton.textContent = "I want another...";
 
     } catch (error) {
         factElement.classList.remove('generated');
+        catBox.classList.remove('yapping');
         void factElement.offsetWidth;
-        factElement.textContent = "Failed to fetch a cat fact. :(";
+        factElement.textContent = "Failed to fetch a cat fact. Nyaw...😿";
         factElement.classList.add('generated');
+        catBox.classList.add('yapping');
+        setTimeout(()=>{
+            catBox.classList.remove('yapping');
+        }, 1690);
         console.error(error);
         meow.play();
         meow.addEventListener('ended', function() {
             meow.remove();
         });
+        genButton.textContent = "I want another...";
     }
 });
